11803be6d1963313b1993fdb6aa01c6983ca5e86
chrom9103 <kdkd21347.tm@gmail.com>
2025-10-31 13:44:48 +0900

    initial commit

---
+++ src/views/MainView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Redirecting to pi-pi-room.vercel.app/works…</div>
+</template>
+
+<script>
+export default {
+  name: 'AoyamafesLinkView',
+  mounted() {
+    // 履歴を残さずリダイレクトする場合は replace を使用
+    window.location.replace('https://pi-pi-room.vercel.app/works');
+  }
+}
+</script>
