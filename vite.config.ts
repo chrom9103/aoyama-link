686f978dd38a8b54e7420b909dc79092b97ee4a4
chrom9103 <kdkd21347.tm@gmail.com>
2025-10-31 13:47:41 +0900

    base URLを'/works/playground/'から'/a/'に変更

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/works/playground/',
+  base: '/a/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
